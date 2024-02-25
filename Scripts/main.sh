cfd1f7abd30530232d9e3e6529c7d3d6d4a423f3
GeekerHWH <hacker.huwenhao@gmail.com>
2024-02-25 20:00:27 +0800

    modulize scripts

--- Scripts/main.sh
+++ Scripts/main.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-source ./modules/setBashrc.sh
-source ./modules/setDir.sh
-source ./modules/setGRUB.sh
+source ./modules/set.sh
 source ./modules/changeMirror.sh
-# tailor is used for debloating gnome-core and installing some powerful tools
-# based on gnome-core
 source ./modules/install.sh
 
 Tsinghua="https://mirrors.tuna.tsinghua.edu.cn/debian"
@@ -13,36 +9,37 @@ Aliyun="https://mirrors.aliyun.com/debian"
 USTC="https://mirrors.ustc.edu.cn/debian"
 
 # main
-# setBashrc
-# setDir
-# changeMirror $Tsinghua "Debian12"
-# tailor
+set_bashrc
+set_dir_structure
+
 read -p "If you want to set GRUB timeout to 0s right now? (N/y): " choice
-# choice="${choice:-N}"
+choice="${choice:-N}"
 if [ $choice == "y" ]; then
     set_grub_time_0
 fi
 
+# tailor is used for debloating gnome-core and installing some powerful tools
+# based on gnome-core
 read -p "If you want tailor right now? (N/y): " choice
-# choice="${choice:-N}"
+choice="${choice:-N}"
 if [ $choice == "y" ]; then
     tailor
 fi
 
 read -p "If you want to install docker right now? (N/y): " choice
-# choice="${choice:-N}"
+choice="${choice:-N}"
 if [ $choice == "y" ]; then
     install_docker
 fi
 
 read -p "If you want to install wineHQ right now? (N/y): " choice
-# choice="${choice:-N}"
+choice="${choice:-N}"
 if [ $choice == "y" ]; then
     install_winehq
 fi
 
 read -p "If you want to reboot right now? (N/y): " choice
-# choice="${choice:-N}"
+choice="${choice:-N}"
 if [ $choice == "y" ]; then
     reboot
 fi
--- Scripts/modules/setGRUB.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function set_grub_time_0() {
-    # change grub timeout to 0 sec
-    sudo sed -i 's/GRUB_TIMEOUT=[0-9]\+/GRUB_TIMEOUT=0/' /etc/default/grub
-    update-grub
-}
